941c74fdafbccd7eed8f917776d70344c2edfe7d
James Vaughan <vaughanj10@gmail.com>
2016-03-16 11:39:48 -0400

    added binary tree and record exercises

--- ch11/constructing.hs
+++ ch11/constructing.hs
@@ -19,3 +19,45 @@ data RecordProduct a b =
 
 idIdentity :: Id (a -> a)
 idIdentity = MkId $ \x -> x
+
+data SocialNetwork =
+    Twitter
+  | AskFm
+  deriving (Eq, Show)
+
+data OperatingSystem =
+    GnuPlusLinux
+  | OpenBSD
+  | Mac
+  | Windows
+  deriving (Eq, Show)
+
+data ProgrammingLanguage =
+    Haskell
+  | Agda
+  | Idris
+  | PureScript
+  deriving (Eq, Show)
+
+data Programmer =
+  Programmer { os :: OperatingSystem
+             , lang :: ProgrammingLanguage }
+  deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux
+  , OpenBSD
+  , Mac
+  , Windows
+  ]
+
+allLanguages :: [ProgrammingLanguage]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers =
+  concatMap (\x ->
+    map (\y ->
+      Programmer { os = x, lang = y }) allLanguages) allOperatingSystems
+
---
+++ ch11/tree.hs
@@ -0,0 +1,47 @@
+data BinaryTree a =
+  Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Show, Eq)
+
+insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
+insert' b Leaf = Node Leaf b Leaf
+insert' b (Node left a right)
+  | b == a = Node left a right
+  | b < a = Node (insert' b left) a right
+  | b > a = Node left a (insert' b right)
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) =
+  Node (mapTree f left) (f a) (mapTree f right)
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testTree' :: BinaryTree Integer
+testTree' =
+  Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+
+mapExpected =
+  Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+mapOkay =
+  if mapTree (+1) testTree' == mapExpected
+     then print "yup okay!"
+     else error "test failed!"
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = concat [[a], preorder left, preorder right]
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = concat [inorder left, [a], inorder right]
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = concat [postorder left, postorder right, [a]]
+
+foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldTree f b bt = foldr f b (preorder bt)
+
+mapTree' :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree' f bt = undefined
