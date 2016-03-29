d3ea7fbc25c0bcc557afb96321b2a7cf13ea8cc3
James Vaughan <vaughanj10@gmail.com>
2016-03-29 17:18:23 -0400

    added Functor instances for Pair, Two, Three data types

--- ch16/Functor.hs
+++ ch16/Functor.hs
@@ -28,3 +28,55 @@ instance (Arbitrary a) => Arbitrary (Pair a) where
     x <- arbitrary
     y <- arbitrary
     return $ Pair x y
+
+propPairId :: Pair [Int] -> Bool
+propPairId x = functorIdentity x
+
+propPairComp :: Pair [Int] -> Bool
+propPairComp x = functorCompose (++ [1]) (reverse) x
+
+-- two
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    return $ Two x y
+
+propTwoId :: Two Bool Int -> Bool
+propTwoId x = functorIdentity x
+
+propTwoComp :: Two Bool Int -> Bool
+propTwoComp x = functorCompose (*3) (+2) x
+
+-- three
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    return $ Three x y z
+
+propThreeId :: Three Bool Char Int -> Bool
+propThreeId x = functorIdentity x
+
+propThreeComp :: Three Bool Char Int -> Bool
+propThreeComp x = functorCompose (*3) (+2) x
+
+main :: IO ()
+main = do
+  quickCheck propPairId
+  quickCheck propPairComp
+  quickCheck propTwoId
+  quickCheck propTwoComp
+  quickCheck propThreeId
+  quickCheck propThreeComp
