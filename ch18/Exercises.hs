625447b7ad910fcbcc4497efe5b9652d150da2b9
James Vaughan <vaughanj10@gmail.com>
2016-04-25 17:10:29 +0200

    finished ch18 and added ch19-20

--- ch18/Exercises.hs
+++ ch18/Exercises.hs
@@ -24,6 +24,10 @@ instance (Eq a) => EqProp (Nope a) where (=-=) = eq
 
 -- LIST
 
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ xs `append` ys
+
 data List a = Nil | Cons a (List a) deriving (Eq, Show)
 
 instance Functor List where
@@ -32,7 +36,35 @@ instance Functor List where
 
 instance Applicative List where
   pure a = Cons a Nil
-  Cons f fs <*> xs = fmap f xs
+  Nil <*> _ = Nil
+  Cons f fs <*> xs = append (fmap f xs) (fs <*> xs)
+
+instance Monad List where
+  return = pure
+  -- (>>=) :: List a -> (a -> List b) -> List b
+  Nil >>= _ = Nil
+  Cons x xs >>= f = append (f x) (xs >>= f)
+
+-- Other
+
+j :: Monad m => m (m a) -> m a
+j xs = xs >>= id
+
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 f xs = fmap f xs
+
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 f xs ys = xs >>= \x -> ys >>= \y -> return (f x y)
+
+l3 :: Monad m => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
+l3 f xs ys zs = xs >>= \x -> ys >>= \y -> zs >>= \z -> return (f x y z)
+
+a :: Monad m => m a -> m (a -> b) -> m b
+a m mf = mf >>= \f -> fmap f m
+
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+-- [1,2,3] -> (\x -> Just (x+1)) -> Just [2,3,4]
+meh xs f = sequence (fmap f xs)
 
 main = do
   quickBatch (monad (NopeDotJpg :: Nope (Int, Int, Int)))
